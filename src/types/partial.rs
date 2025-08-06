6cbfcde0156a2450f6aa9e7cd17a09646ec7abb7
Yak <y@kuhi.to>
2025-08-06 10:06:21 -0700

    update code w/ important check

--- src/types/partial.rs
+++ src/types/partial.rs
@@ -6,7 +6,7 @@ use chia_wallet_sdk::types::Mod;
 use clvm_traits::{FromClvm, ToClvm};
 use hex_literal::hex;
 
-pub const PARTIAL_PUZZLE: [u8; 393] = hex!(
+pub const PARTIAL_PUZZLE: [u8; 420] = hex!(
     "
     ff02ffff01ff04ffff04ff14ffff04ffff0bff0bffff02ff1effff04ff02ffff
     04ffff04ff82013fffff04ffff04ff17ffff04ffff02ffff03ffff15ff82017f
@@ -14,19 +14,20 @@ pub const PARTIAL_PUZZLE: [u8; 393] = hex!(
     808080ff808080ff8080808080ff808080ffff04ffff04ff08ffff04ffff30ff
     82013fffff02ff05ffff04ff8202bfff8203ff8080ff8205bf80ff808080ffff
     04ffff02ff16ffff04ff02ffff04ff8202bfffff04ffff11ff8205bfffff05ff
-    ff14ffff12ff82017fff819f80ff81df808080ff8080808080ffff03ff8202ff
-    ffff04ffff04ff1cff8202ff80ff2f80ff2f80808080ffff04ffff01ffff46ff
-    3f33ff01ffff03ffff15ff0bff8080ffff04ff1cffff04ff05ffff04ff0bffff
-    04ffff04ff05ff8080ff8080808080ffff04ff0aff808080ff02ffff03ffff07
-    ff0580ffff01ff0bffff0102ffff02ff1effff04ff02ffff04ff09ff80808080
-    ffff02ff1effff04ff02ffff04ff0dff8080808080ffff01ff0bffff0101ff05
-    8080ff0180ff018080
+    ff14ffff12ff82017fff819f80ff81df808080ff8080808080ffff03ffff02ff
+    ff03ff8202ffffff01ff15ff820affffff0181ff80ff8080ff0180ffff04ffff
+    04ff1cff8202ff80ff2f80ff2f80808080ffff04ffff01ffff46ff3f33ff01ff
+    ff03ffff15ff0bff8080ffff04ff1cffff04ff05ffff04ff0bffff04ffff04ff
+    05ff8080ff8080808080ffff04ff0aff808080ff02ffff03ffff07ff0580ffff
+    01ff0bffff0102ffff02ff1effff04ff02ffff04ff09ff80808080ffff02ff1e
+    ffff04ff02ffff04ff0dff8080808080ffff01ff0bffff0101ff058080ff0180
+    ff018080
     "
 );
 
 pub const PARTIAL_PUZZLE_HASH: TreeHash = TreeHash::new(hex!(
     "
-    2e0446b44e4522c767fd51a03ab39e792bd0a08b04ee65d50af6126ad8142228
+    83dc065cf7f261338123f8ec988d092aa577af1ab3d2ab9dd37edde13e709cd9
     "
 ));
 
