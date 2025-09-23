1249a3a42be0963dcf6f64b4cf2f9071f9aade79
Yak <y@kuhi.to>
2025-09-23 09:31:38 +0800

    update partial puzzle

--- src/types/partial.rs
+++ src/types/partial.rs
@@ -6,28 +6,28 @@ use chia_wallet_sdk::types::Mod;
 use clvm_traits::{FromClvm, ToClvm};
 use hex_literal::hex;
 
-pub const PARTIAL_PUZZLE: [u8; 420] = hex!(
+pub const PARTIAL_PUZZLE: [u8; 438] = hex!(
     "
     ff02ffff01ff04ffff04ff14ffff04ffff0bff0bffff02ff1effff04ff02ffff
-    04ffff04ff82013fffff04ffff04ff17ffff04ffff02ffff03ffff15ff82017f
-    ff8080ffff0182017fffff01ff088080ff0180ffff04ffff04ff17ff8080ff80
+    04ffff04ff82027fffff04ffff04ff17ffff04ffff02ffff03ffff15ff8202ff
+    ff5f80ffff018202ffffff01ff088080ff0180ffff04ffff04ff17ff8080ff80
     808080ff808080ff8080808080ff808080ffff04ffff04ff08ffff04ffff30ff
-    82013fffff02ff05ffff04ff8202bfff8203ff8080ff8205bf80ff808080ffff
-    04ffff02ff16ffff04ff02ffff04ff8202bfffff04ffff11ff8205bfffff05ff
-    ff14ffff12ff82017fff819f80ff81df808080ff8080808080ffff03ffff02ff
-    ff03ff8202ffffff01ff15ff820affffff0181ff80ff8080ff0180ffff04ffff
-    04ff1cff8202ff80ff2f80ff2f80808080ffff04ffff01ffff46ff3f33ff01ff
-    ff03ffff15ff0bff8080ffff04ff1cffff04ff05ffff04ff0bffff04ffff04ff
-    05ff8080ff8080808080ffff04ff0aff808080ff02ffff03ffff07ff0580ffff
-    01ff0bffff0102ffff02ff1effff04ff02ffff04ff09ff80808080ffff02ff1e
-    ffff04ff02ffff04ff0dff8080808080ffff01ff0bffff0101ff058080ff0180
-    ff018080
+    82027fffff02ff05ffff04ff82057fff820bff8080ff820b7f80ff808080ffff
+    04ffff02ff16ffff04ff02ffff04ff82057fffff04ffff11ff820b7fffff05ff
+    ff14ffff12ff8202ffff82013f80ff8201bf808080ff8080808080ffff03ffff
+    02ffff03ff8205ffffff01ff15ff8215ffffff0181ff80ff8080ff0180ffff04
+    ffff04ff1cff8205ff80ffff02ff2fff820fff8080ffff02ff2fff820fff8080
+    808080ffff04ffff01ffff46ff3f33ff01ffff03ffff15ff0bff8080ffff04ff
+    1cffff04ff05ffff04ff0bffff04ffff04ff05ff8080ff8080808080ffff04ff
+    0aff808080ff02ffff03ffff07ff0580ffff01ff0bffff0102ffff02ff1effff
+    04ff02ffff04ff09ff80808080ffff02ff1effff04ff02ffff04ff0dff808080
+    8080ffff01ff0bffff0101ff058080ff0180ff018080
     "
 );
 
 pub const PARTIAL_PUZZLE_HASH: TreeHash = TreeHash::new(hex!(
     "
-    83dc065cf7f261338123f8ec988d092aa577af1ab3d2ab9dd37edde13e709cd9
+    0962ad16fcb5e9a609ac22f8c80b55976c86ef96d7c82291e1d7edcf99def8d8
     "
 ));
 
@@ -41,22 +41,24 @@ pub struct PartialPriceData {
 
 #[derive(ToClvm, FromClvm, Debug, Clone, Copy, PartialEq, Eq)]
 #[clvm(curry)]
-pub struct PartialPuzzleArgs<CM, IC> {
+pub struct PartialPuzzleArgs<CM, IP> {
     pub cat_maker: CM,
     pub other_asset_offer_mod: Bytes32,
     pub receiver_puzzle_hash: Bytes32,
-    pub inner_conditions: IC,
+    pub inner_puzzle: IP,
+    pub min_other_asset_amount_minus_one: u64,
     pub price_data: PartialPriceData,
 }
 
 #[derive(FromClvm, ToClvm, Debug, Clone, PartialEq, Eq)]
 #[clvm(list)]
-pub struct PartialSolution<CCR, CMS> {
+pub struct PartialSolution<CCR, CMS, IPS> {
     pub my_data: CoinProof,
     pub other_asset_amount: u64,
     pub create_coin_rest: Option<CCR>,
-    #[clvm(rest)]
     pub cat_maker_solution: CMS,
+    #[clvm(rest)]
+    pub inner_puzzle_solution: IPS,
 }
 
 impl<CM, IC> Mod for PartialPuzzleArgs<CM, IC> {
