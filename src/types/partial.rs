baca3c925c2b2f67cded467064af19008d3278ae
Yak <y@kuhi.to>
2025-09-30 09:36:43 +0800

    optimize opcodes

--- src/types/partial.rs
+++ src/types/partial.rs
@@ -6,28 +6,28 @@ use chia_wallet_sdk::types::Mod;
 use clvm_traits::{FromClvm, ToClvm};
 use hex_literal::hex;
 
-pub const PARTIAL_PUZZLE: [u8; 448] = hex!(
+pub const PARTIAL_PUZZLE: [u8; 446] = hex!(
     "
-    ff02ffff01ff04ffff04ff14ffff04ffff0bff0bffff02ff1effff04ff02ffff
-    04ffff04ff82027fffff04ffff04ff17ffff04ffff02ffff03ffff15ff8202ff
-    ff5f80ffff018202ffffff01ff088080ff0180ffff04ffff04ff17ff8080ff80
-    808080ff808080ff8080808080ff808080ffff04ffff04ff08ffff04ffff30ff
-    82027fffff02ff05ffff04ff82057fff820bff8080ff820b7f80ff808080ffff
-    04ffff02ff16ffff04ff02ffff04ff82057fffff04ffff11ff820b7fffff05ff
-    ff14ffff12ff8202ffff82013f80ff8201bf808080ff8080808080ffff02ffff
-    03ffff02ffff03ff8205ffffff01ff15ff8215ffffff0181ff80ff8080ff0180
-    ffff01ff04ffff04ff1cff8205ff80ffff02ff2fff820fff8080ffff01ff02ff
-    2fff820fff8080ff0180808080ffff04ffff01ffff46ff3f33ff01ffff03ffff
-    15ff0bff8080ffff04ff1cffff04ff05ffff04ff0bffff04ffff04ff05ff8080
-    ff8080808080ffff04ff0aff808080ff02ffff03ffff07ff0580ffff01ff0bff
-    ff0102ffff02ff1effff04ff02ffff04ff09ff80808080ffff02ff1effff04ff
-    02ffff04ff0dff8080808080ffff01ff0bffff0101ff058080ff0180ff018080
+    ff02ffff01ff04ffff04ffff013fffff04ffff0bff0bffff02ff06ffff04ff02
+    ffff04ffff04ff82027fffff04ffff04ff17ffff04ffff02ffff03ffff15ff82
+    02ffff5f80ffff018202ffffff01ff088080ff0180ffff04ffff04ff17ff8080
+    ff80808080ff808080ff8080808080ff808080ffff04ffff04ffff0146ffff04
+    ffff30ff82027fffff02ff05ffff04ff82057fff820bff8080ff820b7f80ff80
+    8080ffff04ffff02ff04ffff04ff02ffff04ff82057fffff04ffff11ff820b7f
+    ffff05ffff14ffff12ff8202ffff82013f80ff8201bf808080ff8080808080ff
+    ff02ffff03ffff02ffff03ff8205ffffff01ff15ff8215ffffff0181ff80ff80
+    80ff0180ffff01ff04ffff04ffff0133ff8205ff80ffff02ff2fff820fff8080
+    ffff01ff02ff2fff820fff8080ff0180808080ffff04ffff01ffff03ffff15ff
+    0bff8080ffff04ffff0133ffff04ff05ffff04ff0bffff04ffff04ff05ff8080
+    ff8080808080ffff01ff018080ff02ffff03ffff07ff0580ffff01ff0bffff01
+    02ffff02ff06ffff04ff02ffff04ff09ff80808080ffff02ff06ffff04ff02ff
+    ff04ff0dff8080808080ffff01ff0bffff0101ff058080ff0180ff018080
     "
 );
 
 pub const PARTIAL_PUZZLE_HASH: TreeHash = TreeHash::new(hex!(
     "
-    fbae8af65e9a2854376cac792f02fa6b0d6a64ce3edf61a063d815a1db3d9e6c
+    91fe76d50effb3b2e7079770f3c309d7c0aa8022b5034457121f61ba38df4b12
     "
 ));
 
