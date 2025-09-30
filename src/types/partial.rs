59fd664ab9bbed5e68eb11eb15f65dfe2bf6169e
Yak <y@kuhi.to>
2025-09-30 08:57:35 +0800

    partial offer puzzle fix

--- src/types/partial.rs
+++ src/types/partial.rs
@@ -6,7 +6,7 @@ use chia_wallet_sdk::types::Mod;
 use clvm_traits::{FromClvm, ToClvm};
 use hex_literal::hex;
 
-pub const PARTIAL_PUZZLE: [u8; 438] = hex!(
+pub const PARTIAL_PUZZLE: [u8; 448] = hex!(
     "
     ff02ffff01ff04ffff04ff14ffff04ffff0bff0bffff02ff1effff04ff02ffff
     04ffff04ff82027fffff04ffff04ff17ffff04ffff02ffff03ffff15ff8202ff
@@ -14,20 +14,20 @@ pub const PARTIAL_PUZZLE: [u8; 438] = hex!(
     808080ff808080ff8080808080ff808080ffff04ffff04ff08ffff04ffff30ff
     82027fffff02ff05ffff04ff82057fff820bff8080ff820b7f80ff808080ffff
     04ffff02ff16ffff04ff02ffff04ff82057fffff04ffff11ff820b7fffff05ff
-    ff14ffff12ff8202ffff82013f80ff8201bf808080ff8080808080ffff03ffff
-    02ffff03ff8205ffffff01ff15ff8215ffffff0181ff80ff8080ff0180ffff04
-    ffff04ff1cff8205ff80ffff02ff2fff820fff8080ffff02ff2fff820fff8080
-    808080ffff04ffff01ffff46ff3f33ff01ffff03ffff15ff0bff8080ffff04ff
-    1cffff04ff05ffff04ff0bffff04ffff04ff05ff8080ff8080808080ffff04ff
-    0aff808080ff02ffff03ffff07ff0580ffff01ff0bffff0102ffff02ff1effff
-    04ff02ffff04ff09ff80808080ffff02ff1effff04ff02ffff04ff0dff808080
-    8080ffff01ff0bffff0101ff058080ff0180ff018080
+    ff14ffff12ff8202ffff82013f80ff8201bf808080ff8080808080ffff02ffff
+    03ffff02ffff03ff8205ffffff01ff15ff8215ffffff0181ff80ff8080ff0180
+    ffff01ff04ffff04ff1cff8205ff80ffff02ff2fff820fff8080ffff01ff02ff
+    2fff820fff8080ff0180808080ffff04ffff01ffff46ff3f33ff01ffff03ffff
+    15ff0bff8080ffff04ff1cffff04ff05ffff04ff0bffff04ffff04ff05ff8080
+    ff8080808080ffff04ff0aff808080ff02ffff03ffff07ff0580ffff01ff0bff
+    ff0102ffff02ff1effff04ff02ffff04ff09ff80808080ffff02ff1effff04ff
+    02ffff04ff0dff8080808080ffff01ff0bffff0101ff058080ff0180ff018080
     "
 );
 
 pub const PARTIAL_PUZZLE_HASH: TreeHash = TreeHash::new(hex!(
     "
-    0962ad16fcb5e9a609ac22f8c80b55976c86ef96d7c82291e1d7edcf99def8d8
+    fbae8af65e9a2854376cac792f02fa6b0d6a64ce3edf61a063d815a1db3d9e6c
     "
 ));
 
